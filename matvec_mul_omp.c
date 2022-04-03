99c63975c46ba0e1f874ff19d40f59e0cdfaf0f8
mitchellgraba <mitchell.graba@protonmail.com>
2022-04-03 17:30:20 -0400

    formatting

--- matvec_mul_omp.c
+++ matvec_mul_omp.c
@@ -3,18 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
   int myid;
   long i, j, m, n;
 
   printf("How many rows?: ");
-  if (scanf("%ld", &m) < 1) {
+  if (scanf("%ld", &m) < 1)
+  {
     printf("Check input for vector length.\n");
     return -1;
   }
 
   printf("How many columns?: ");
-  if (scanf("%ld", &n) < 1) {
+  if (scanf("%ld", &n) < 1)
+  {
     printf("Check input for vector length.\n");
     return -1;
   }
@@ -31,27 +34,33 @@ int main() {
 
   /* Some initializations */
 
-  for (i = 0; i < m; i++) {
+  for (i = 0; i < m; i++)
+  {
     result[i] = 0.0;
   }
 
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < m; j++) {
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < m; j++)
+    {
       mat[j][i] = (float)(1.0); //(float)j / (i + 1);
     }
   }
 
   printf("[");
-  // Just the inner loop should be parrallized 
-  for (j = 0; j < m; j++) {
+  // Just the inner loop should be parrallized
+  for (j = 0; j < m; j++)
+  {
     printf("Outerloop: I am thread %d\n\n", omp_get_thread_num());
 
-    #pragma omp parallel for default(shared) schedule(dynamic) reduction(+ : result[:m])
-    for (i = 0; i < n; i++) {
+#pragma omp parallel for default(shared) schedule(dynamic) reduction(+ \
+                                                                     : result[:m])
+    for (i = 0; i < n; i++)
+    {
       result[j] += (mat[j][i] * vec[i]);
       printf("Innerloop: I am thread %d, working on row %ld column %ld \n\n", omp_get_thread_num(), j, i);
     }
-    #pragma omp barrier
+#pragma omp barrier
   }
   for (i = 0; i < m; i++)
     printf("%f,", result[i]);
